80b0039f3cfed93817e5273dae251f7460264b53
Huy Nguyen <huy@huy-nguyen.com>
2014-08-26 19:57:33 -0400

    Added scripts that use unequalTimeGF_full

---
+++ ED_01_full.m
@@ -0,0 +1,16 @@
+%% For running on SciClone
+format compact;
+
+Lx=9; % The number of lattice sites in the x direction
+
+N_up=5; % The number of spin-up electrons
+N_dn=5; % The number of spin-down electrons
+
+U=4.0; % The on-site repulsion strength in the Hubbard Hamiltonian
+t=1; % The hopping amplitude between nearest-neighbor sites in the x direction
+
+tau = 2.5;
+
+maxNumCompThreads(8)
+
+[ exactUp, exactDn ] = unequalTimeGF_full( t, U, tau, Lx, N_up, N_dn );
