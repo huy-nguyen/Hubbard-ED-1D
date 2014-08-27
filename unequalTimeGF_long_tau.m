6d629760988ba9fb750d0cb5fe30deb9ff2aaeee
Huy Nguyen <huy@huy-nguyen.com>
2014-08-27 19:31:02 -0400

    Added check such that if the # eigenvalues requested is larger than the dimension of the matrix in either the up or down sector, that number will be adjusted appropriately and individually for each sector.

--- unequalTimeGF_long_tau.m
+++ unequalTimeGF_long_tau.m
@@ -4,6 +4,8 @@ function [ spinUpGreenFunction, spinDnGreenFunction ] = unequalTimeGF_long_tau( 
 OPTS.issym = 1;
 OPTS.isreal = 1;
 
+expanded_space_size_up = nchoosek(noOfSites,noOfUp+1)*nchoosek(noOfSites,noOfDn);
+expanded_space_size_dn = nchoosek(noOfSites,noOfUp)*nchoosek(noOfSites,noOfDn + 1);
 format compact;
 savedFileName=strcat('ED_',int2str(noOfSites),'_sites_',int2str(noOfUp),'u',int2str(noOfDn),'d_U_',num2str(U, '%4.2f'),'_tau_',num2str(tau, '%4.2f'),'_t_',num2str(t),' ',datestr(now,'_yymmdd_HHMMSS'),'.mat')
 
@@ -20,9 +22,16 @@ if (noOfUp < noOfSites) && (noOfDn < noOfSites)
     disp('Saved groundState groundStateEnergy'); % for debugging
        
 %% SPIN UP:
-    sizeSpacePlusOne=nchoosek(noOfSites,noOfUp+1)*nchoosek(noOfSites,noOfDn);  % might not be needed. 
+%     sizeSpacePlusOne=nchoosek(noOfSites,noOfUp+1)*nchoosek(noOfSites,noOfDn);  % might not be needed. 
+    if NUM_OF_EIGEN_VALUES >= expanded_space_size_up
+        NUM_OF_EIGEN_VALUES_UP = expanded_space_size_up - 1;
+        fprintf('NUM_EIGEN_VALUES exceeds dimension of spin-up matrix. Now set to %d\n', NUM_OF_EIGEN_VALUES_UP)
+    else
+        NUM_OF_EIGEN_VALUES_UP = NUM_OF_EIGEN_VALUES;
+    end
+    
     [eigenVectors, eigenValues] = eigs( hubbardHamiltonian( t, U, noOfSites, noOfUp+1, noOfDn ), ...
-                                            NUM_OF_EIGEN_VALUES, 'sa', OPTS);
+                                            NUM_OF_EIGEN_VALUES_UP, 'sa', OPTS);
     eigenValues = diag(eigenValues);
     for i_site=1:noOfSites        
         destructionMatrix=creationOperator( noOfSites, noOfUp, noOfDn , i_site, 'up' )'; 
@@ -31,7 +40,7 @@ if (noOfUp < noOfSites) && (noOfDn < noOfSites)
             right_wave_function = creationOperator( noOfSites, noOfUp, noOfDn , j_site, 'up' ) * ...
                                                 groundState;
             k_sum = 0;
-            for k_eigenValues = 1:NUM_OF_EIGEN_VALUES %sum over k
+            for k_eigenValues = 1:NUM_OF_EIGEN_VALUES_UP %sum over k
                 expo_factor = exp( tau*( groundStateEnergy - eigenValues(k_eigenValues)));                
                 i_total = left_wave_function * eigenVectors(:,k_eigenValues);
                 j_total = dot( right_wave_function, conj(eigenVectors(:,k_eigenValues)) );
@@ -48,9 +57,16 @@ if (noOfUp < noOfSites) && (noOfDn < noOfSites)
     
     clearvars sizeSpacePlusOne eigenVectors eigenValues;
 %% SPIN DOWN:    
-    sizeSpacePlusOne=nchoosek(noOfSites,noOfUp)*nchoosek(noOfSites,noOfDn + 1);  % might not be needed. 
+%     sizeSpacePlusOne=nchoosek(noOfSites,noOfUp)*nchoosek(noOfSites,noOfDn + 1);  % might not be needed.  
+    if NUM_OF_EIGEN_VALUES >= expanded_space_size_dn
+        NUM_OF_EIGEN_VALUES_DN = expanded_space_size_dn - 1;
+        fprintf('NUM_EIGEN_VALUES exceeds dimension of spin-down matrix. Now set to %d\n', NUM_OF_EIGEN_VALUES_DN)
+    else
+        NUM_OF_EIGEN_VALUES_DN = NUM_OF_EIGEN_VALUES;
+    end
+
     [eigenVectors, eigenValues] = eigs( hubbardHamiltonian( t, U, noOfSites, noOfUp, noOfDn + 1 ), ...
-                                            NUM_OF_EIGEN_VALUES, 'sa', OPTS);
+                                            NUM_OF_EIGEN_VALUES_DN, 'sa', OPTS);
     eigenValues = diag(eigenValues);
     for i_site=1:noOfSites        
         destructionMatrix=creationOperator( noOfSites, noOfUp, noOfDn , i_site, 'dn' )'; 
@@ -59,7 +75,7 @@ if (noOfUp < noOfSites) && (noOfDn < noOfSites)
             right_wave_function = creationOperator( noOfSites, noOfUp, noOfDn , j_site, 'dn' ) * ...
                                                 groundState;
             k_sum = 0;
-            for k_eigenValues = 1:NUM_OF_EIGEN_VALUES %sum over k
+            for k_eigenValues = 1:NUM_OF_EIGEN_VALUES_DN %sum over k
                 expo_factor = exp( tau*( groundStateEnergy - eigenValues(k_eigenValues)));                
                 i_total = left_wave_function * eigenVectors(:,k_eigenValues);
                 j_total = dot( right_wave_function, conj(eigenVectors(:,k_eigenValues)) );
