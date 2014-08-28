e0aa4a9a5af5d6aa79c7f615cf8e3e1fa06cd7a2
Huy Nguyen (PC) <huy@huy-nguyen.com>
2014-08-28 19:49:21 -0400

    Added loop over different taus

--- unequalTimeGF_long_tau.m
+++ unequalTimeGF_long_tau.m
@@ -17,7 +17,9 @@ savedFileName=strcat('ED_',num2str(noOfSites, '%02d'),...
                     '_eigen_', num2str(NUM_OF_EIGEN_VALUES, '%04d'),...
                     ' ',datestr(now,'_yymmdd_HHMMSS'),'.mat');
 
+pause(2);
 output_files = {};
+list_of_taus = tau_start:tau_step:tau_end;
 for i_filename = tau_start:tau_step:tau_end
     output_files{i_filename} = strcat('ED_',num2str(noOfSites, '%02d'),...
         '_sites_',num2str(noOfUp, '%02d'),...
@@ -57,53 +59,62 @@ if (noOfUp < noOfSites) && (noOfDn < noOfSites)
                                             NUM_OF_EIGEN_VALUES_UP, 'sa', OPTS);
     eigenValues_up = diag(eigenValues_up);
     save(savedFileName,'-append','eigenValues_up','eigenVectors_up');
-    fprintf('Begin calculating off-diagonal elements at time %s.\n', datestr(now,'yymmdd_HHMMSS'))
-    for i_site=1:noOfSites        
-        fprintf('Working on i = %3d     at time %s\n', i_site, datestr(now,'yymmdd_HHMMSS'))
-        destructionMatrix=creationOperator( noOfSites, noOfUp, noOfDn , i_site, 'up' )'; 
-        left_wave_function =  (groundState') * destructionMatrix; 
-        for j_site=(i_site+1):noOfSites % maybe consider j_site=i_site:noOfSites ?
-            fprintf('Working on j =     %3d at time %s\n', j_site, datestr(now,'yymmdd_HHMMSS'))
-            right_wave_function = creationOperator( noOfSites, noOfUp, noOfDn , j_site, 'up' ) * ...
-                                                groundState;
-            k_sum = 0;
-            for k_eigenValues = 1:NUM_OF_EIGEN_VALUES_UP %sum over k
-                expo_factor = exp( tau*( groundStateEnergy - eigenValues_up(k_eigenValues)));                
-                i_total = left_wave_function * eigenVectors_up(:,k_eigenValues);
-                j_total = dot( right_wave_function, conj(eigenVectors_up(:,k_eigenValues)) );
-                k_sum = k_sum + expo_factor * i_total * j_total;
-                
-                clearvars expo_factor i_total j_total;
+    
+    for t_tau = 1:length(output_files) % loop over taus
+        
+        tau = list_of_taus(t_tau);
+        spinUpGreenFunction=zeros(noOfSites);
+        
+        fprintf('Begin calculating off-diagonal elements at time %s.\n', datestr(now,'yymmdd_HHMMSS'))
+        for i_site=1:noOfSites        
+            fprintf('Working on i = %3d     at time %s\n', i_site, datestr(now,'yymmdd_HHMMSS'))
+            destructionMatrix=creationOperator( noOfSites, noOfUp, noOfDn , i_site, 'up' )'; 
+            left_wave_function =  (groundState') * destructionMatrix; 
+            for j_site=(i_site+1):noOfSites % maybe consider j_site=i_site:noOfSites ?
+                fprintf('Working on j =     %3d at time %s\n', j_site, datestr(now,'yymmdd_HHMMSS'))
+                right_wave_function = creationOperator( noOfSites, noOfUp, noOfDn , j_site, 'up' ) * ...
+                                                    groundState;
+                k_sum = 0;
+                for k_eigenValues = 1:NUM_OF_EIGEN_VALUES_UP %sum over k
+                    expo_factor = exp( tau*( groundStateEnergy - eigenValues_up(k_eigenValues)));                
+                    i_total = left_wave_function * eigenVectors_up(:,k_eigenValues);
+                    j_total = dot( right_wave_function, conj(eigenVectors_up(:,k_eigenValues)) );
+                    k_sum = k_sum + expo_factor * i_total * j_total;
+
+                    clearvars expo_factor i_total j_total;
+                end
+                spinUpGreenFunction(i_site,j_site) = k_sum;
+                clearvars creationMatrix right_wave_function k_sum;
             end
-            spinUpGreenFunction(i_site,j_site) = k_sum;
-            clearvars creationMatrix right_wave_function k_sum;
+            clearvars destructionMatrix left_wave_function;
         end
-        clearvars destructionMatrix left_wave_function;
+
+        fprintf('Begin calculating on-diagonal elements at time  %s.\n', datestr(now,'yymmdd_HHMMSS'))
+        i_site = 1;
+            destructionMatrix=creationOperator( noOfSites, noOfUp, noOfDn , i_site, 'up' )';
+            left_wave_function =  (groundState') * destructionMatrix;    
+            j_site = 1;  
+                right_wave_function = creationOperator( noOfSites, noOfUp, noOfDn , j_site, 'up' ) * ...
+                    groundState;
+                k_sum = 0;
+                for k_eigenValues = 1:NUM_OF_EIGEN_VALUES_UP %sum over k
+                    expo_factor = exp( tau*( groundStateEnergy - eigenValues_up(k_eigenValues)));
+                    i_total = left_wave_function * eigenVectors_up(:,k_eigenValues);
+                    j_total = dot( right_wave_function, conj(eigenVectors_up(:,k_eigenValues)) );
+                    k_sum = k_sum + expo_factor * i_total * j_total;        
+                    clearvars expo_factor i_total j_total;
+                end
+                diagonal_elem_up = k_sum;
+
+        spinUpGreenFunction = spinUpGreenFunction + spinUpGreenFunction';
+        for i_diag = 1:noOfSites
+            spinUpGreenFunction(i_diag, i_diag) = diagonal_elem_up;
+        end
+
+        clearvars diagonal_elem_up;
+        save(output_files{t_tau}, 'spinUpGreenFunction');
     end
-    
-    fprintf('Begin calculating on-diagonal elements at time  %s.\n', datestr(now,'yymmdd_HHMMSS'))
-    i_site = 1;
-        destructionMatrix=creationOperator( noOfSites, noOfUp, noOfDn , i_site, 'up' )';
-        left_wave_function =  (groundState') * destructionMatrix;    
-        j_site = 1;  
-            right_wave_function = creationOperator( noOfSites, noOfUp, noOfDn , j_site, 'up' ) * ...
-                groundState;
-            k_sum = 0;
-            for k_eigenValues = 1:NUM_OF_EIGEN_VALUES_UP %sum over k
-                expo_factor = exp( tau*( groundStateEnergy - eigenValues_up(k_eigenValues)));
-                i_total = left_wave_function * eigenVectors_up(:,k_eigenValues);
-                j_total = dot( right_wave_function, conj(eigenVectors_up(:,k_eigenValues)) );
-                k_sum = k_sum + expo_factor * i_total * j_total;        
-                clearvars expo_factor i_total j_total;
-            end
-            diagonal_elem_up = k_sum;
-    
-    spinUpGreenFunction = spinUpGreenFunction + spinUpGreenFunction';
-    for i_diag = 1:noOfSites
-        spinUpGreenFunction(i_diag, i_diag) = diagonal_elem_up;
-    end
-    
-    clearvars sizeSpacePlusOne eigenVectors_up eigenValues_up diagonal_elem_up;
+    clearvars sizeSpacePlusOne eigenVectors_up eigenValues_up ;
     
 %% SPIN DOWN:        
     fprintf('Begin spin-down calculations at time %s.\n', datestr(now,'yymmdd_HHMMSS'))
@@ -118,53 +129,62 @@ if (noOfUp < noOfSites) && (noOfDn < noOfSites)
                                             NUM_OF_EIGEN_VALUES_DN, 'sa', OPTS);
     eigenValues_dn = diag(eigenValues_dn);
     save(savedFileName,'-append','eigenVectors_dn','eigenValues_dn');
-    fprintf('Begin calculating off-diagonal elements at time %s.\n', datestr(now,'yymmdd_HHMMSS'))
-    for i_site=1:noOfSites 
-        fprintf('Working on i = %3d     at time %s\n', i_site, datestr(now,'yymmdd_HHMMSS'))
-        destructionMatrix=creationOperator( noOfSites, noOfUp, noOfDn , i_site, 'dn' )'; 
-        left_wave_function =  (groundState') * destructionMatrix; 
-        for j_site=(i_site+1):noOfSites % maybe consider j_site=i_site:noOfSites ?
-            fprintf('Working on j =     %3d at time %s\n', j_site, datestr(now,'yymmdd_HHMMSS'))
-            right_wave_function = creationOperator( noOfSites, noOfUp, noOfDn , j_site, 'dn' ) * ...
-                                                groundState;
-            k_sum = 0;
-            for k_eigenValues = 1:NUM_OF_EIGEN_VALUES_DN %sum over k
-                expo_factor = exp( tau*( groundStateEnergy - eigenValues_dn(k_eigenValues)));                
-                i_total = left_wave_function * eigenVectors_dn(:,k_eigenValues);
-                j_total = dot( right_wave_function, conj(eigenVectors_dn(:,k_eigenValues)) );
-                k_sum = k_sum + expo_factor * i_total * j_total;
-                
-                clearvars expo_factor i_total j_total;
+
+    for t_tau = 1:length(output_files) % loop over taus
+        
+        tau = list_of_taus(t_tau);
+        spinDnGreenFunction=zeros(noOfSites);    
+    
+        fprintf('Begin calculating off-diagonal elements at time %s.\n', datestr(now,'yymmdd_HHMMSS'))
+        for i_site=1:noOfSites 
+            fprintf('Working on i = %3d     at time %s\n', i_site, datestr(now,'yymmdd_HHMMSS'))
+            destructionMatrix=creationOperator( noOfSites, noOfUp, noOfDn , i_site, 'dn' )'; 
+            left_wave_function =  (groundState') * destructionMatrix; 
+            for j_site=(i_site+1):noOfSites % maybe consider j_site=i_site:noOfSites ?
+                fprintf('Working on j =     %3d at time %s\n', j_site, datestr(now,'yymmdd_HHMMSS'))
+                right_wave_function = creationOperator( noOfSites, noOfUp, noOfDn , j_site, 'dn' ) * ...
+                                                    groundState;
+                k_sum = 0;
+                for k_eigenValues = 1:NUM_OF_EIGEN_VALUES_DN %sum over k
+                    expo_factor = exp( tau*( groundStateEnergy - eigenValues_dn(k_eigenValues)));                
+                    i_total = left_wave_function * eigenVectors_dn(:,k_eigenValues);
+                    j_total = dot( right_wave_function, conj(eigenVectors_dn(:,k_eigenValues)) );
+                    k_sum = k_sum + expo_factor * i_total * j_total;
+
+                    clearvars expo_factor i_total j_total;
+                end
+                spinDnGreenFunction(i_site,j_site) = k_sum;
+                clearvars creationMatrix right_wave_function;
             end
-            spinDnGreenFunction(i_site,j_site) = k_sum;
-            clearvars creationMatrix right_wave_function;
+            clearvars destructionMatrix left_wave_function;
         end
-        clearvars destructionMatrix left_wave_function;
+
+        fprintf('Begin calculating on-diagonal elements at time  %s.\n', datestr(now,'yymmdd_HHMMSS'))
+        i_site = 1;
+            destructionMatrix=creationOperator( noOfSites, noOfUp, noOfDn , i_site, 'dn' )';
+            left_wave_function =  (groundState') * destructionMatrix;    
+            j_site = 1;  
+                right_wave_function = creationOperator( noOfSites, noOfUp, noOfDn , j_site, 'dn' ) * ...
+                    groundState;
+                k_sum = 0;
+                for k_eigenValues = 1:NUM_OF_EIGEN_VALUES_DN %sum over k
+                    expo_factor = exp( tau*( groundStateEnergy - eigenValues_dn(k_eigenValues)));
+                    i_total = left_wave_function * eigenVectors_dn(:,k_eigenValues);
+                    j_total = dot( right_wave_function, conj(eigenVectors_dn(:,k_eigenValues)) );
+                    k_sum = k_sum + expo_factor * i_total * j_total;        
+                    clearvars expo_factor i_total j_total;
+                end
+                diagonal_elem_dn = k_sum;
+
+        spinDnGreenFunction = spinDnGreenFunction + spinDnGreenFunction';
+        for i_diag = 1:noOfSites
+            spinDnGreenFunction(i_diag, i_diag) = diagonal_elem_dn;
+        end
+
+        clearvars diagonal_elem_dn;
+        save(output_files{t_tau}, '-append', 'spinDnGreenFunction');
     end
-    
-    fprintf('Begin calculating on-diagonal elements at time  %s.\n', datestr(now,'yymmdd_HHMMSS'))
-    i_site = 1;
-        destructionMatrix=creationOperator( noOfSites, noOfUp, noOfDn , i_site, 'dn' )';
-        left_wave_function =  (groundState') * destructionMatrix;    
-        j_site = 1;  
-            right_wave_function = creationOperator( noOfSites, noOfUp, noOfDn , j_site, 'dn' ) * ...
-                groundState;
-            k_sum = 0;
-            for k_eigenValues = 1:NUM_OF_EIGEN_VALUES_DN %sum over k
-                expo_factor = exp( tau*( groundStateEnergy - eigenValues_dn(k_eigenValues)));
-                i_total = left_wave_function * eigenVectors_dn(:,k_eigenValues);
-                j_total = dot( right_wave_function, conj(eigenVectors_dn(:,k_eigenValues)) );
-                k_sum = k_sum + expo_factor * i_total * j_total;        
-                clearvars expo_factor i_total j_total;
-            end
-            diagonal_elem_dn = k_sum;
-    
-    spinDnGreenFunction = spinDnGreenFunction + spinDnGreenFunction';
-    for i_diag = 1:noOfSites
-        spinDnGreenFunction(i_diag, i_diag) = diagonal_elem_dn;
-    end
-    
-    clearvars sizeSpacePlusOne eigenVectors_dn eigenValues_dn diagonal_elem_dn;
+    clearvars sizeSpacePlusOne eigenVectors_dn eigenValues_dn;
 
 
 else
