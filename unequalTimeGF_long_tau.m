2473a78326a42fed81d7528b6c1d18ae76fc1a4a
Huy Nguyen (PC) <huy@huy-nguyen.com>
2014-08-29 12:10:21 -0400

    - Optimized memory usage by offloading large arrays to disk as early as   possible and only load them into memory when needed. - Added 2 new data points that will be saved along with the mat file: the   method of computation ('short_tau' vs 'long_tau') and the sha1 hash of   the version of the files used in the computation.

--- unequalTimeGF_long_tau.m
+++ unequalTimeGF_long_tau.m
@@ -1,4 +1,4 @@
-function output_files = unequalTimeGF_long_tau( t, U, tau_start, tau_end, tau_step, noOfSites, noOfUp, noOfDn, NUM_OF_EIGEN_VALUES )
+function output_files = unequalTimeGF_long_tau( t, U, tau_start, tau_end, tau_step, noOfSites, noOfUp, noOfDn, NUM_OF_EIGEN_VALUES, method, commit_number )
 % calculate the unequal time GF by using series expansion
 
 OPTS.issym = 1;
@@ -21,7 +21,17 @@ for i_filename = 1:length(list_of_taus)
                                         '_eigen_', num2str(NUM_OF_EIGEN_VALUES, '%04d'),...
                                         ' ',datestr(now,'_yymmdd_HHMMSS'),'.mat');
 end
-                
+        
+
+aux_file_name = strcat('aux_',num2str(noOfSites, '%02d'),...
+                                    '_sites_',num2str(noOfUp, '%02d'),...
+                                    'u',num2str(noOfDn, '%02d'),...
+                                    'd_U_',num2str(U, '%4.2f'),...
+                                    '_tau_',num2str(tau, '%4.2f'),...
+                                    '_t_',num2str(t),...
+                                    '_eigen_', num2str(NUM_OF_EIGEN_VALUES, '%04d'),...
+                                    ' ',datestr(now,'_yymmdd_HHMMSS'),'.mat');
+                                
 tic;
 
 if (noOfUp < noOfSites) && (noOfDn < noOfSites)
@@ -30,10 +40,14 @@ if (noOfUp < noOfSites) && (noOfDn < noOfSites)
     [groundState,groundStateEnergy]=eigs( hubbardHamiltonian( t, U, noOfSites, noOfUp, noOfDn ),...
                                                1,'sa'); %ASSUMING THAT THE HAMILTONIAN IS REAL SYMMETRIC
                                            
+    save( aux_file_name, 'groundState', 'groundStateEnergy', '-mat', '-v7.3');
+    aux_file = matfile(aux_file_name);
+    
     for i_f = 1:length(output_files)
         save(output_files{i_f},'groundState','groundStateEnergy');            
     end     
     clearvars i_f;
+    clearvars groundState;
     
     fprintf('%03d data files:\n', length(list_of_taus))
     for i_dat_files = 1:length(list_of_taus)
@@ -52,12 +66,13 @@ if (noOfUp < noOfSites) && (noOfDn < noOfSites)
     [eigenVectors_up, eigenValues_up] = eigs( hubbardHamiltonian( t, U, noOfSites, noOfUp+1, noOfDn ), ...
                                             NUM_OF_EIGEN_VALUES_UP, 'sa', OPTS);
     eigenValues_up = diag(eigenValues_up);
+    fprintf('Done with diagonalization at time %s.\n', datestr(now,'yymmdd_HHMMSS'))
     
+    save( aux_file_name, '-append', 'eigenVectors_up', 'eigenValues_up', '-mat', '-v7.3');
     for i_f = 1:length(output_files)
         save(output_files{i_f},'-append','eigenValues_up','eigenVectors_up');            
     end     
-    clearvars i_f;
-    fprintf('Done with diagonalization at time %s.\n', datestr(now,'yymmdd_HHMMSS'))
+    clearvars i_f eigenVectors_up;
     for t_tau = 1:length(output_files) % loop over taus
     
         tau = list_of_taus(t_tau);
@@ -68,38 +83,55 @@ if (noOfUp < noOfSites) && (noOfDn < noOfSites)
         for i_site=1:noOfSites        
             fprintf('        Working on i = %3d     at time %s\n', i_site, datestr(now,'yymmdd_HHMMSS'))
             destructionMatrix=creationOperator( noOfSites, noOfUp, noOfDn , i_site, 'up' )'; 
-            left_wave_function =  (groundState') * destructionMatrix; 
+            left_wave_function =  (aux_file.groundState') * destructionMatrix; 
+            save( aux_file_name, '-append', 'left_wave_function', '-mat', '-v7.3');
+            clearvars left_wave_function;
             for j_site=(i_site+1):noOfSites 
                 fprintf('        Working on j =     %3d at time %s\n', j_site, datestr(now,'yymmdd_HHMMSS'))
                 right_wave_function = creationOperator( noOfSites, noOfUp, noOfDn , j_site, 'up' ) * ...
-                                                    groundState;
+                                                    aux_file.groundState;
+                save( aux_file_name,'-append',  'right_wave_function', '-mat', '-v7.3');
+                clearvars right_wave_function;
+                
                 k_sum = 0;
                 for k_eigenValues = 1:NUM_OF_EIGEN_VALUES_UP %sum over k
                     expo_factor = exp( tau*( groundStateEnergy - eigenValues_up(k_eigenValues)));                
-                    i_total = left_wave_function * eigenVectors_up(:,k_eigenValues);
-                    j_total = dot( right_wave_function, conj(eigenVectors_up(:,k_eigenValues)) );
+                    i_total = aux_file.left_wave_function * aux_file.eigenVectors_up(:,k_eigenValues);
+                    j_total = dot( aux_file.right_wave_function, conj(aux_file.eigenVectors_up(:,k_eigenValues)) );
                     k_sum = k_sum + expo_factor * i_total * j_total;
 
                     clearvars expo_factor i_total j_total;
                 end
                 spinUpGreenFunction(i_site,j_site) = k_sum;
                 clearvars creationMatrix right_wave_function k_sum;
+                right_wave_function = [];
+                save( aux_file_name, '-append', 'right_wave_function', '-mat', '-v7.3');
+                creationMatrix = []; % clear creationMatrix from mat file
+                save( aux_file_name, '-append', 'creationMatrix', '-mat', '-v7.3');
+                
             end
             clearvars destructionMatrix left_wave_function;
+            destructionMatrix = [];
+            left_wave_function = [];
+            save( aux_file_name, '-append', 'destructionMatrix', 'left_wave_function', '-mat', '-v7.3');
         end
 
         fprintf('        Begin calculating on-diagonal elements at time  %s.\n', datestr(now,'yymmdd_HHMMSS'))
         i_site = 1;
             destructionMatrix=creationOperator( noOfSites, noOfUp, noOfDn , i_site, 'up' )';
-            left_wave_function =  (groundState') * destructionMatrix;    
+            left_wave_function =  (aux_file.groundState') * destructionMatrix; 
+            save( aux_file_name,'-append',  'left_wave_function', '-mat', '-v7.3');
+            clearvars left_wave_function;   
             j_site = 1;  
                 right_wave_function = creationOperator( noOfSites, noOfUp, noOfDn , j_site, 'up' ) * ...
-                    groundState;
+                    aux_file.groundState;
+                save( aux_file_name, '-append', 'right_wave_function', '-mat', '-v7.3');
+                clearvars right_wave_function;
                 k_sum = 0;
                 for k_eigenValues = 1:NUM_OF_EIGEN_VALUES_UP %sum over k
                     expo_factor = exp( tau*( groundStateEnergy - eigenValues_up(k_eigenValues)));
-                    i_total = left_wave_function * eigenVectors_up(:,k_eigenValues);
-                    j_total = dot( right_wave_function, conj(eigenVectors_up(:,k_eigenValues)) );
+                    i_total = aux_file.left_wave_function * aux_file.eigenVectors_up(:,k_eigenValues);
+                    j_total = dot( aux_file.right_wave_function, conj(aux_file.eigenVectors_up(:,k_eigenValues)) );
                     k_sum = k_sum + expo_factor * i_total * j_total;        
                     clearvars expo_factor i_total j_total;
                 end
@@ -109,6 +141,13 @@ if (noOfUp < noOfSites) && (noOfDn < noOfSites)
         for i_diag = 1:noOfSites
             spinUpGreenFunction(i_diag, i_diag) = diagonal_elem_up;
         end
+        
+        
+        clearvars creationMatrix right_wave_function k_sum;
+        right_wave_function = [];
+        save( aux_file_name, '-append', 'right_wave_function', '-mat', '-v7.3');
+        creationMatrix = []; % clear creationMatrix from mat file
+        save( aux_file_name, '-append', 'creationMatrix', '-mat', '-v7.3');
 
         clearvars diagonal_elem_up;
         save(output_files{t_tau}, '-append', 'spinUpGreenFunction');
@@ -129,13 +168,13 @@ if (noOfUp < noOfSites) && (noOfDn < noOfSites)
     [eigenVectors_dn, eigenValues_dn] = eigs( hubbardHamiltonian( t, U, noOfSites, noOfUp, noOfDn + 1 ), ...
                                             NUM_OF_EIGEN_VALUES_DN, 'sa', OPTS);
     eigenValues_dn = diag(eigenValues_dn);
-    
+    fprintf('Done with diagonalization at time %s.\n', datestr(now,'yymmdd_HHMMSS'))
+    save( aux_file_name, '-append', 'eigenVectors_dn', 'eigenVectors_dn', '-mat', '-v7.3');
     for i_f = 1:length(output_files)
         save(output_files{i_f},'-append','eigenVectors_dn','eigenValues_dn');            
     end     
-    clearvars i_f;
+    clearvars i_f eigenVectors_dn;
 
-    fprintf('Done with diagonalization at time %s.\n', datestr(now,'yymmdd_HHMMSS'))
     for t_tau = 1:length(output_files) % loop over taus
         
         tau = list_of_taus(t_tau);
@@ -146,38 +185,54 @@ if (noOfUp < noOfSites) && (noOfDn < noOfSites)
         for i_site=1:noOfSites 
             fprintf('        Working on i = %3d     at time %s\n', i_site, datestr(now,'yymmdd_HHMMSS'))
             destructionMatrix=creationOperator( noOfSites, noOfUp, noOfDn , i_site, 'dn' )'; 
-            left_wave_function =  (groundState') * destructionMatrix; 
+            left_wave_function =  (aux_file.groundState') * destructionMatrix; 
+            save( aux_file_name, '-append', 'left_wave_function', '-mat', '-v7.3');
+            clearvars left_wave_function;
+            
             for j_site=(i_site+1):noOfSites 
                 fprintf('        Working on j =     %3d at time %s\n', j_site, datestr(now,'yymmdd_HHMMSS'))
                 right_wave_function = creationOperator( noOfSites, noOfUp, noOfDn , j_site, 'dn' ) * ...
-                                                    groundState;
+                                                    aux_file.groundState;
+                save( aux_file_name,'-append',  'right_wave_function', '-mat', '-v7.3');
+                clearvars right_wave_function;
                 k_sum = 0;
                 for k_eigenValues = 1:NUM_OF_EIGEN_VALUES_DN %sum over k
                     expo_factor = exp( tau*( groundStateEnergy - eigenValues_dn(k_eigenValues)));                
-                    i_total = left_wave_function * eigenVectors_dn(:,k_eigenValues);
-                    j_total = dot( right_wave_function, conj(eigenVectors_dn(:,k_eigenValues)) );
+                    i_total = aux_file.left_wave_function * aux_file.eigenVectors_dn(:,k_eigenValues);
+                    j_total = dot( aux_file.right_wave_function, conj(aux_file.eigenVectors_dn(:,k_eigenValues)) );
                     k_sum = k_sum + expo_factor * i_total * j_total;
 
                     clearvars expo_factor i_total j_total;
                 end
                 spinDnGreenFunction(i_site,j_site) = k_sum;
                 clearvars creationMatrix right_wave_function;
+                right_wave_function = [];
+                save( aux_file_name,'-append',  'right_wave_function', '-mat', '-v7.3');
+                creationMatrix = []; % clear creationMatrix from mat file
+                save( aux_file_name,'-append',  'creationMatrix', '-mat', '-v7.3');
             end
             clearvars destructionMatrix left_wave_function;
+            destructionMatrix = [];
+            left_wave_function = [];
+            save( aux_file_name, '-append', 'destructionMatrix', 'left_wave_function', '-mat', '-v7.3');
         end
 
         fprintf('    Begin calculating on-diagonal elements at time  %s.\n', datestr(now,'yymmdd_HHMMSS'))
         i_site = 1;
             destructionMatrix=creationOperator( noOfSites, noOfUp, noOfDn , i_site, 'dn' )';
-            left_wave_function =  (groundState') * destructionMatrix;    
+            left_wave_function =  (aux_file.groundState') * destructionMatrix; 
+            save( aux_file_name,'-append',  'left_wave_function', '-mat', '-v7.3');
+            clearvars left_wave_function;      
             j_site = 1;  
                 right_wave_function = creationOperator( noOfSites, noOfUp, noOfDn , j_site, 'dn' ) * ...
-                    groundState;
+                    aux_file.groundState;
+                save( aux_file_name, '-append', 'right_wave_function', '-mat', '-v7.3');
+                clearvars right_wave_function;
                 k_sum = 0;
                 for k_eigenValues = 1:NUM_OF_EIGEN_VALUES_DN %sum over k
                     expo_factor = exp( tau*( groundStateEnergy - eigenValues_dn(k_eigenValues)));
-                    i_total = left_wave_function * eigenVectors_dn(:,k_eigenValues);
-                    j_total = dot( right_wave_function, conj(eigenVectors_dn(:,k_eigenValues)) );
+                    i_total = aux_file.left_wave_function * aux_file.eigenVectors_dn(:,k_eigenValues);
+                    j_total = dot( aux_file.right_wave_function, conj(aux_file.eigenVectors_dn(:,k_eigenValues)) );
                     k_sum = k_sum + expo_factor * i_total * j_total;        
                     clearvars expo_factor i_total j_total;
                 end
@@ -188,6 +243,12 @@ if (noOfUp < noOfSites) && (noOfDn < noOfSites)
             spinDnGreenFunction(i_diag, i_diag) = diagonal_elem_dn;
         end
 
+        clearvars creationMatrix right_wave_function k_sum;
+        right_wave_function = [];
+        save( aux_file_name, '-append', 'right_wave_function', '-mat', '-v7.3');
+        creationMatrix = []; % clear creationMatrix from mat file
+        save( aux_file_name, '-append', 'creationMatrix', '-mat', '-v7.3');
+        
         clearvars diagonal_elem_dn;
         save(output_files{t_tau}, '-append', 'spinDnGreenFunction');
         clearvars spinDnGreenFunction;
@@ -204,7 +265,7 @@ time=toc
     
 for i_f = 1:length(output_files)
     tau = list_of_taus(i_f);
-    save(output_files{i_f},'-append','noOfSites','noOfUp','noOfDn','U','tau','t','time', 'NUM_OF_EIGEN_VALUES_UP', 'NUM_OF_EIGEN_VALUES_DN');            
+    save(output_files{i_f},'-append','noOfSites','noOfUp','noOfDn','U','tau','t','time', 'NUM_OF_EIGEN_VALUES_UP', 'NUM_OF_EIGEN_VALUES_DN', 'method', 'commit_number');            
 end     
 fprintf('Finish all calculations at time %s.\n', datestr(now,'yymmdd_HHMMSS'))
 
